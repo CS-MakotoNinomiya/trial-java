f00566531ad8bc05bcab26be6fa8c09d16e81f7b
Ninomiya <m.ninomiya@central-soft.co.jp>
2019-02-08 18:50:23 +0900

    ArrayListの実験

---
+++ trial-java-zzz/src/main/java/trial/arraylist/TrialArrayList.java
@@ -0,0 +1,13 @@
+package trial.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrialArrayList {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add(null);
+        System.out.println(list.size());
+    }
+}
